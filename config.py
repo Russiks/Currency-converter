22ec14df56979c47cf4fae43dc5cd0e72000548c
Руслан Морозов <117517806+Russiks@users.noreply.github.com>
2023-01-14 22:54:43 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+exchanges = {  # Создаем словарь с валютами доступными пользователю для конвертации
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+
+headers = {
+    "apikey": "RfeFJ3jLcVOYevESQOhw1cLMf4aaiGaJ"  # Ключ API Key с сайта apilayer.com
+}
+
+TOKEN = "5905347853:AAHOcUQC_CIkJr2ALBbo0nDYezx96QFcOYg"  # Константа токена бота
+
